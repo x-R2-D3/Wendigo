7840d3c1903b5f2a534d11c557ed6b23c7c1e557
R2-D3 <isackboy07@gmail.com>
2025-04-24 02:15:16 -0500

    Amazing UI stuffs

---
+++ Wendigo/Assets/HapticsScene.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HapticsScene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        // Load the game scene (index 1) and unload the current scene (index 0)
+
+
+
+        SceneManager.LoadSceneAsync(4);
+        SceneManager.UnloadSceneAsync(0);
+
+    }
+}
